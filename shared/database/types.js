f0de5d6552743080b8e97181389151a13d9ebbf6
chelojimenez <marcelojimenezrocabado@gmail.com>
2025-07-05 16:08:08 -0700

    refactor db changes

--- shared/database/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Shared database types for MCP Jam Inspector
- * These types mirror the database schema and provide type safety
- */
-Object.defineProperty(exports, "__esModule", { value: true });
